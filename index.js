b86c615c2ecae5646ecc47dda16f0fa10ce720ad
antinarkoticheskayaploshadka-stack <antinarkoticheskaya.ploshadka@gmail.com>
2026-04-14 12:56:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-telegram.WebApp.downloadFile("https://github.com/antinarkoticheskayaploshadka-stack/AntiNarkApp/releases/download/noalco/materialy.o.vrede.alkogolya.zip");
+        let td = window.Telegram.WebApp;
+        let get = document.getElementById("get");
+        let alco = document.getElementById("alco");
+
+        get.addEventListener("click", () => {
+            document.getElementById("main").style.display = "none";
+            document.getElementById("menu").style.display = "block";
+        });
+
+        alco.addEventListener("click", () => {
+            document.getElementById("menu").style.display = "none";
+            document.getElementById("order").style.display = "block";
+        })
