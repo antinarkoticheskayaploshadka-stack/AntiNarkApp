6f86f45793b57d1d38690b79e82dd53c35368e6d
antinarkoticheskayaploshadka-stack <antinarkoticheskaya.ploshadka@gmail.com>
2026-05-05 15:41:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
         let td = window.Telegram.WebApp;
         let get = document.getElementById("get");
         let alco = document.getElementById("alco");
+        let narco = document.getElementById("narco");
+        let vape = document.getElementById("vape");
+        let energy = document.getElementById("energy");
 
         get.addEventListener("click", () => {
             document.getElementById("main").style.display = "none";
@@ -9,5 +12,20 @@
 
         alco.addEventListener("click", () => {
             document.getElementById("menu").style.display = "none";
-            document.getElementById("order").style.display = "block";
-        })
+            document.getElementById("order1").style.display = "block";
+        });
+
+        narco.addEventListener("click", () => {
+            document.getElementById("menu").style.display = "none";
+            document.getElementById("order2").style.display = "block";
+        });
+
+        vape.addEventListener("click", () => {
+            document.getElementById("menu").style.display = "none";
+            document.getElementById("order3").style.display = "block";
+        });
+
+        energy.addEventListener("click", () => {
+            document.getElementById("menu").style.display = "none";
+            document.getElementById("order4").style.display = "block";
+        });
